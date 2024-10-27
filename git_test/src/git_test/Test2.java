a8352daa5ebc043f4b02d57b8da13a076ebec8b9
kimjinseok <souk2109@naver.com>
2024-10-27 18:55:53 +0900

    test

--- git_test/src/git_test/Test2.java
+++ git_test/src/git_test/Test2.java
@@ -1,7 +1,7 @@
 package git_test;
 
-public class TestClass1 {
+public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("hi");
+		System.out.println("dwdw");
 	}
 }
--- git_test/src/git_test/Test4.java
+++
@@ -1,7 +0,0 @@
-package git_test;
-
-public class Test4 extends TestClass1 {
-	public static void main(String[] args) {
-		System.out.println("dw");
-	}
-}
